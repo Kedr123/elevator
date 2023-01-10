1d8133b3c6422d070ae0aca8be04c3e66c33c356
Kedr123 <48621337+Kedr123@users.noreply.github.com>
2023-01-10 21:19:20 +0300

    initialization of the project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>Привет, мир!</div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
+
