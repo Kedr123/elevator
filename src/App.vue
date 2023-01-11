964194ed155d903e564ab2d3415fdbba41aa02ea
Kedr123 <48621337+Kedr123@users.noreply.github.com>
2023-01-11 12:29:40 +0300

    create component Floors

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,26 @@
     <div class="container">
         <div class="elevators">
             <elevator :floors = "floors" :destinationFloor = "destinationFloor" :id="1" @update="checkElevator"/>
-        </div>  
+           
+        </div> 
+        <floors :floors = "floors" :queue = "queue"/>  
     </div>
     
 </template>
 
 <script>
-    import Elevator from "@/components/Elevator.vue"
+    import Elevator from "@/components/Elevator.vue";
+    import Floors from "@/components/Floors.vue";
 
     export default {
         components:{
-            Elevator,
+            Elevator, Floors,
         },
         data(){
             return {
                 destinationFloor:1,
                 floors:5,
+                queue:[]
             }
         },
         methods:{
@@ -34,6 +38,7 @@
     display: flex;
     height: 100%;
     align-items: center;
+    width: clamp(100px,calc(100%/5),150px);
 }
 
 body{
@@ -41,10 +46,12 @@ body{
 }
 
 .container{
-    padding: 2%;
+    /* padding: 2%; */
+    margin-left: 2%;
     display: flex;
     align-items: center;
     height: 100vh;
+    /* width: 100vw; */
 }
 </style>
 
---
+++ src/components/Floors.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="floors" >
+        <button  v-for="i in floors" class="button" :class="{active:queue.includes(i)}">
+            {{ i }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        floors:{},
+        queue:{}
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        buttonClick(id){
+            if(queue.includes(id)){
+                return;
+            }
+
+            this.$emit("append", id)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .floors{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        height: 90%;
+        margin-left: 3%;
+    }
+
+    .button{
+        padding: 50%;
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        background-color: white;
+        border: 2px solid blue;
+    }
+
+    .active{
+        background-color: red;
+    }
+
+</style>
